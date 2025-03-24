b7f9c6f2735c0a4eba5005ae6fd0b3c6f2dc9fe7
Smudge <poms17226@gmail.com>
2025-03-23 23:52:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# GuideSense​ - Spring 2025
+<img width="280" hight="280" alt="Afziya" src="https://github.com/user-attachments/assets/8feb4562-1009-4786-be11-a3b240118d8e">
+
+***
+## Project Description
+This innovative web app leverages computer vision and machine learning to provide real-time audio guidance, identifying objects, obstacles, and crosswalks for the blind and visually impaired. ​
+
+Combining GPS and AI object detection, it aims to make it easier and safer for blind people to get around independently, especially in cities and outdoor areas. ​
+
+The goal is to provide a helpful, accessible, and dependable way for visually impaired people to navigate.​
+
+**Key features of the application include:​**
+
+Real-time Object Detection ​
+
+Voice Navigation and Alerts​
+
+GPS Integration​
+
+User-Friendly Interface
+
---
+++ frontend/src/index.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ClerkProvider } from '@clerk/clerk-react'
+import {  RedirectToSignIn, SignIn, SignUp, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+
+
+const clerk_key=process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+console.log(clerk_key);
+if(!clerk_key){
+  throw new Error("Key WAS NOT FOUND");
+}
+
+const ClerkWithRoutes = () => {
+
+
+  const navigate = useNavigate();
+ 
+
+  return (
+    <ClerkProvider publishableKey={clerk_key} navigate={(to) => navigate(to)}>
+      <Routes>
+      <Route
+          path="/*"
+          element={
+            <>
+              <SignedIn>
+                <App />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+
+        {/* Sign-in and Sign-up routes */}
+        <Route
+          path="/sign-in/*"
+          element={
+            <SignIn
+              afterSignInUrl="/"
+              afterSignOutUrl="/sign-in"
+              routing="path"
+              path="/sign-in"
+            />
+          }
+        />
+        <Route
+          path="/sign-up/*"
+          element={
+            <SignUp
+              afterSignUpUrl="/"
+              routing="path"
+              path="/sign-up"
+            />
+          }
+        />
+       
+      </Routes>
+    </ClerkProvider>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <ClerkProvider publishableKey={clerk_key}>
+      <App />
+    </ClerkProvider> */}
+    <BrowserRouter>
+      <ClerkWithRoutes />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
